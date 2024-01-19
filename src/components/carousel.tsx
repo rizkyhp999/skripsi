dd71537e4142ba3afc99feacb107faf1e10876aa
rizkyhp999 <rizkyhutamaputra@gmail.com>
2024-01-20 00:31:10 +0700

    3 c

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -33,7 +33,7 @@ export default function carousel() {
               <div className=" absolute inset-x-0 m-auto flex flex-wrap items-center justify-center lg:static  lg:my-auto">
                 <div className=" bg-white p-5 ">
                   <h1 className="text-sm sm:text-lg md:text-xl lg:text-3xl font-bold ">
-                    Bahasa Daerah
+                    Bahasa Daerah 1
                   </h1>
                   <div className="text-sm sm:text-lg md:text-xl lg:text-xl text-justify ">
                     Bahasa daerah di Indonesia berjumlah ratusan. Badan
@@ -50,14 +50,70 @@ export default function carousel() {
               </div>
             </div>
           </SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>
+            <div className=" flex flex-wrap lg:flex-nowrap items-center justify-center">
+              <img
+                width={500}
+                height={500}
+                src="/carousel/1.png"
+                alt=""
+                className="relative lg:w-1/2 lg:h-auto lg:static flex justify-center blur-sm lg:blur-0"
+              />
+              <div className=" absolute inset-x-0 m-auto flex flex-wrap items-center justify-center lg:static  lg:my-auto">
+                <div className=" bg-white p-5 ">
+                  <h1 className="text-sm sm:text-lg md:text-xl lg:text-3xl font-bold ">
+                    Bahasa Daerah 2
+                  </h1>
+                  <div className="text-sm sm:text-lg md:text-xl lg:text-xl text-justify ">
+                    Bahasa daerah di Indonesia berjumlah ratusan. Badan
+                    Pengembangan dan Pembinaan Bahasa (Badan Bahasa) hingga 2019
+                    telah memverifikasi sebanyak 718 bahasa daerah—bukan dialek
+                    atau subdialek. Jumlah bahasa tersebut diperoleh berdasarkan
+                    hasil pengolahan data pemetaan bahasa yang diambil di 2.560
+                    daerah pengamatan (DP) di seluruh Indonesia yang dilakukan
+                    sejak tahun 1992. Jumlah tersebut tentunya akan bertambah
+                    seiring dengan bertambahnya jumlah daerah pengamatan dalam
+                    pemetaan berikutnya.{" "}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className=" flex flex-wrap lg:flex-nowrap items-center justify-center">
+              <img
+                width={500}
+                height={500}
+                src="/carousel/1.png"
+                alt=""
+                className="relative lg:w-1/2 lg:h-auto lg:static flex justify-center blur-sm lg:blur-0"
+              />
+              <div className=" absolute inset-x-0 m-auto flex flex-wrap items-center justify-center lg:static  lg:my-auto">
+                <div className=" bg-white p-5 ">
+                  <h1 className="text-sm sm:text-lg md:text-xl lg:text-3xl font-bold ">
+                    Bahasa Daerah 3
+                  </h1>
+                  <div className="text-sm sm:text-lg md:text-xl lg:text-xl text-justify ">
+                    Bahasa daerah di Indonesia berjumlah ratusan. Badan
+                    Pengembangan dan Pembinaan Bahasa (Badan Bahasa) hingga 2019
+                    telah memverifikasi sebanyak 718 bahasa daerah—bukan dialek
+                    atau subdialek. Jumlah bahasa tersebut diperoleh berdasarkan
+                    hasil pengolahan data pemetaan bahasa yang diambil di 2.560
+                    daerah pengamatan (DP) di seluruh Indonesia yang dilakukan
+                    sejak tahun 1992. Jumlah tersebut tentunya akan bertambah
+                    seiring dengan bertambahnya jumlah daerah pengamatan dalam
+                    pemetaan berikutnya.{" "}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          {/* <SwiperSlide>Slide 4</SwiperSlide>
           <SwiperSlide>Slide 5</SwiperSlide>
           <SwiperSlide>Slide 6</SwiperSlide>
           <SwiperSlide>Slide 7</SwiperSlide>
           <SwiperSlide>Slide 8</SwiperSlide>
-          <SwiperSlide>Slide 9</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide> */}
         </Swiper>
       </div>
     </>
