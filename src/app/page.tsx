1559e1ee1a5d7d91c31f4c339ee5a52d8839e30e
rizkyhp999 <rizkyhutamaputra@gmail.com>
2024-01-20 01:54:18 +0700

    T

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Carousel from "@/components/carousel";
+import TextBeranda from "@/components/textBeranda";
 export default function Home() {
   return (
     <>
       <Carousel></Carousel>
+      <TextBeranda></TextBeranda>
     </>
   );
 }
