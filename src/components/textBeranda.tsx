3fbb1daa5c62b288cf68f15f2a60cfcf2380eb5f
rizkyhp999 <rizkyhutamaputra@gmail.com>
2024-01-21 01:16:33 +0700

    1920px

--- src/components/textBeranda.tsx
+++ src/components/textBeranda.tsx
@@ -4,7 +4,7 @@ import { animate, motion, useAnimation, useInView } from "framer-motion";
 import { AnimasiMuncul } from "@/components/animasi";
 export default function TextBeranda() {
   return (
-    <div className=" bg-primer w-full ">
+    <div className=" bg-primer w-[1920px] ">
       <div className="container mx-auto">
         <AnimasiMuncul>
           <h1 className="text-white text-lg text-justify font-bold mx-5 mt-8 md:text-2xl lg:text-4xl md:px-[20px] lg:px-[50px] md:mt-[25px] lg:pt-[75px]">
