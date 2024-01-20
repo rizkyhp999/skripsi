82d6c526a46b3f7d6dae32bb5cce36f19d1facd5
rizkyhp999 <rizkyhutamaputra@gmail.com>
2024-01-20 15:54:30 +0700

    remove div carousel

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -41,23 +41,21 @@ export default function Carousel() {
                 alt=""
                 className="relative md:hidden lg:w-1/2 lg:h-auto lg:static justify-center blur-sm  lg:blur-0"
               />
-              <div className=" absolute inset-x-0 m-auto flex flex-wrap items-center justify-center lg:static  lg:my-auto">
-                <div className=" bg-white p-5 ">
-                  <h1 className="text-sm sm:text-lg md:text-xl lg:text-3xl font-bold ">
-                    Bahasa Daerah 1
-                  </h1>
-                  <div className="text-sm sm:text-lg md:text-xl lg:text-xl text-justify ">
-                    Bahasa daerah di Indonesia berjumlah ratusan. Badan
-                    Pengembangan dan Pembinaan Bahasa (Badan Bahasa) hingga 2019
-                    telah memverifikasi sebanyak 718 bahasa daerah—bukan dialek
-                    atau subdialek. Jumlah bahasa tersebut diperoleh berdasarkan
-                    hasil pengolahan data pemetaan bahasa yang diambil di 2.560
-                    daerah pengamatan (DP) di seluruh Indonesia yang dilakukan
-                    sejak tahun 1992. Jumlah tersebut tentunya akan bertambah
-                    seiring dengan bertambahnya jumlah daerah pengamatan dalam
-                    pemetaan berikutnya.{" "}
-                  </div>
-                </div>
+              <div className=" absolute bg-white p-5  inset-x-0 m-auto flex flex-wrap items-center justify-center lg:static  lg:my-auto">
+                <h1 className="text-sm sm:text-lg md:text-xl lg:text-3xl font-bold ">
+                  Bahasa Daerah 1
+                </h1>
+                <p className="text-sm sm:text-lg md:text-xl lg:text-xl text-justify ">
+                  Bahasa daerah di Indonesia berjumlah ratusan. Badan
+                  Pengembangan dan Pembinaan Bahasa (Badan Bahasa) hingga 2019
+                  telah memverifikasi sebanyak 718 bahasa daerah—bukan dialek
+                  atau subdialek. Jumlah bahasa tersebut diperoleh berdasarkan
+                  hasil pengolahan data pemetaan bahasa yang diambil di 2.560
+                  daerah pengamatan (DP) di seluruh Indonesia yang dilakukan
+                  sejak tahun 1992. Jumlah tersebut tentunya akan bertambah
+                  seiring dengan bertambahnya jumlah daerah pengamatan dalam
+                  pemetaan berikutnya.{" "}
+                </p>
               </div>
             </div>
           </SwiperSlide>
@@ -77,23 +75,21 @@ export default function Carousel() {
                 alt=""
                 className="relative md:hidden lg:w-1/2 lg:h-auto lg:static justify-center blur-sm  lg:blur-0"
               />
-              <div className=" absolute inset-x-0 m-auto flex flex-wrap items-center justify-center lg:static  lg:my-auto">
-                <div className=" bg-white p-5 ">
-                  <h1 className="text-sm sm:text-lg md:text-xl lg:text-3xl font-bold ">
-                    Bahasa Daerah 2
-                  </h1>
-                  <div className="text-sm sm:text-lg md:text-xl lg:text-xl text-justify ">
-                    Bahasa daerah di Indonesia berjumlah ratusan. Badan
-                    Pengembangan dan Pembinaan Bahasa (Badan Bahasa) hingga 2019
-                    telah memverifikasi sebanyak 718 bahasa daerah—bukan dialek
-                    atau subdialek. Jumlah bahasa tersebut diperoleh berdasarkan
-                    hasil pengolahan data pemetaan bahasa yang diambil di 2.560
-                    daerah pengamatan (DP) di seluruh Indonesia yang dilakukan
-                    sejak tahun 1992. Jumlah tersebut tentunya akan bertambah
-                    seiring dengan bertambahnya jumlah daerah pengamatan dalam
-                    pemetaan berikutnya.{" "}
-                  </div>
-                </div>
+              <div className=" absolute bg-white p-5  inset-x-0 m-auto flex flex-wrap items-center justify-center lg:static  lg:my-auto">
+                <h1 className="text-sm sm:text-lg md:text-xl lg:text-3xl font-bold ">
+                  Bahasa Daerah 2
+                </h1>
+                <p className="text-sm sm:text-lg md:text-xl lg:text-xl text-justify ">
+                  Bahasa daerah di Indonesia berjumlah ratusan. Badan
+                  Pengembangan dan Pembinaan Bahasa (Badan Bahasa) hingga 2019
+                  telah memverifikasi sebanyak 718 bahasa daerah—bukan dialek
+                  atau subdialek. Jumlah bahasa tersebut diperoleh berdasarkan
+                  hasil pengolahan data pemetaan bahasa yang diambil di 2.560
+                  daerah pengamatan (DP) di seluruh Indonesia yang dilakukan
+                  sejak tahun 1992. Jumlah tersebut tentunya akan bertambah
+                  seiring dengan bertambahnya jumlah daerah pengamatan dalam
+                  pemetaan berikutnya.{" "}
+                </p>
               </div>
             </div>
           </SwiperSlide>
@@ -113,23 +109,21 @@ export default function Carousel() {
                 alt=""
                 className="relative md:hidden lg:w-1/2 lg:h-auto lg:static justify-center blur-sm  lg:blur-0"
               />
-              <div className=" absolute inset-x-0 m-auto flex flex-wrap items-center justify-center lg:static  lg:my-auto">
-                <div className=" bg-white p-5 ">
-                  <h1 className="text-sm sm:text-lg md:text-xl lg:text-3xl font-bold ">
-                    Bahasa Daerah 3
-                  </h1>
-                  <div className="text-sm sm:text-lg md:text-xl lg:text-xl text-justify ">
-                    Bahasa daerah di Indonesia berjumlah ratusan. Badan
-                    Pengembangan dan Pembinaan Bahasa (Badan Bahasa) hingga 2019
-                    telah memverifikasi sebanyak 718 bahasa daerah—bukan dialek
-                    atau subdialek. Jumlah bahasa tersebut diperoleh berdasarkan
-                    hasil pengolahan data pemetaan bahasa yang diambil di 2.560
-                    daerah pengamatan (DP) di seluruh Indonesia yang dilakukan
-                    sejak tahun 1992. Jumlah tersebut tentunya akan bertambah
-                    seiring dengan bertambahnya jumlah daerah pengamatan dalam
-                    pemetaan berikutnya.{" "}
-                  </div>
-                </div>
+              <div className=" absolute bg-white p-5  inset-x-0 m-auto flex flex-wrap items-center justify-center lg:static  lg:my-auto">
+                <h1 className="text-sm sm:text-lg md:text-xl lg:text-3xl font-bold ">
+                  Bahasa Daerah 3
+                </h1>
+                <p className="text-sm sm:text-lg md:text-xl lg:text-xl text-justify ">
+                  Bahasa daerah di Indonesia berjumlah ratusan. Badan
+                  Pengembangan dan Pembinaan Bahasa (Badan Bahasa) hingga 2019
+                  telah memverifikasi sebanyak 718 bahasa daerah—bukan dialek
+                  atau subdialek. Jumlah bahasa tersebut diperoleh berdasarkan
+                  hasil pengolahan data pemetaan bahasa yang diambil di 2.560
+                  daerah pengamatan (DP) di seluruh Indonesia yang dilakukan
+                  sejak tahun 1992. Jumlah tersebut tentunya akan bertambah
+                  seiring dengan bertambahnya jumlah daerah pengamatan dalam
+                  pemetaan berikutnya.{" "}
+                </p>
               </div>
             </div>
           </SwiperSlide>
