19c7862d42210892033c6797001a6f4005e5a9bb
rizkyhp999 <rizkyhutamaputra@gmail.com>
2024-01-22 01:51:49 +0700

    peta

--- src/app/peta/page.tsx
+++ src/app/peta/page.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import Peta from "@/components/peta/peta";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <>
+      <div className="container justify-center items-center mx-auto">
+        <Peta></Peta>
+        {/* <img src="indonesiaHigh.svg" alt="" width={1000} height={1000} /> */}
+      </div>
+    </>
+  );
 }
