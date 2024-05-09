f77ad9a47a21a907096d44aa5cc2990ea7020996
rizkyhp999 <rizkyhutamaputra@gmail.com>
2024-05-09 15:06:35 +0700

    textBeranda

--- src/components/textBeranda.tsx
+++ src/components/textBeranda.tsx
@@ -2,30 +2,36 @@ import React from "react";
 
 export default function textBeranda() {
   return (
-    <div className="  ">
-      <div className="container mx-auto w-auto h-auto px-12 z-10 ">
-        <h1 className="text-white text-lg text-justify font-bold ">
+    <>
+      <div className="container mx-auto w-auto h-auto px-5 z-10 lg:px-[100px]">
+        <h1 className="text-white text-2xl text-justify font-semibold lg:text-4xl ">
           Menurut UNESCO dalam{" "}
-          <span className="text-sekunder">30 tahun terakhir</span>, 200 bahasa
-          daerah di dunia <span className="text-sekunder">punah</span>. Di
-          Indonesia terdapat{" "}
+          <span className="text-sekunder">30 tahun terakhir</span>,
+          <span className="text-sekunder"> 200 bahasa daerah</span> di dunia
+          <span className="text-sekunder"> punah</span>. Di Indonesia terdapat{" "}
           <span className="text-sekunder">718 bahasa daerah.</span> Namun,
           banyak kondisinya terancam{" "}
           <span className="text-sekunder">punah</span> dan{" "}
           <span className="text-sekunder">kritis</span>.
         </h1>
-        <h1 className="text-white text-lg text-justify font-bold  ">
+        <br />
+        <br />
+        <h1 className="text-white text-2xl text-justify font-semibold lg:text-4xl">
           Penyebab bahasa daerah <span className="text-sekunder">punah </span>
           adalah penutur jati yang tak lagi menggunakan dan mewariskan bahasanya
           ke generasi berikutnya.
         </h1>
       </div>
-      <div className="mx-auto w-auto h-auto px-12 md:hidden ">
+      <br />
+      <br />
+      <div className="mx-auto w-auto h-auto px-5 sm:hidden  ">
         <img src="bubblechatsm.svg" alt="" className=" " />
       </div>
-      <div className="mx-auto w-auto h-auto hidden md:block ">
+      <div className="mx-auto w-auto h-auto hidden sm:block  lg:px-[100px]">
         <img src="bubblechatlg.svg" alt="" className=" " />
       </div>
-    </div>
+      <br />
+      <br />
+    </>
   );
 }
