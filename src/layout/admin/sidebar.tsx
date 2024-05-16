8e00fa08fa8432f0a1d7bf880e762b94de654a4e
rizkyhp999 <rizkyhutamaputra@gmail.com>
2024-05-16 13:00:13 +0700

    1

--- src/layout/admin/sidebar.tsx
+++ src/layout/admin/sidebar.tsx
@@ -57,16 +57,13 @@ export default function Sidebar({ children }: SidebarProps) {
               </button>
 
               {/* Logo */}
-              <a
-                href="/admin/dashboard"
-                className="flex ml-2 hidden sm:block md:mr-24"
-              >
+              <a href="/" className="flex ml-2  md:mr-24">
                 <img src="/logo.png" className="h-8 mr-3" alt="" />
               </a>
             </div>
             {/* Right Side (User/Actions) */}
             <div className="flex items-center">
-              <div className="flex items-center ml-3 text-white">awawawa</div>
+              <div className="flex items-center ml-3 text-white">Keluar</div>
             </div>
           </div>
         </div>
@@ -90,14 +87,19 @@ export default function Sidebar({ children }: SidebarProps) {
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
                 <svg
-                  className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                  className="w-5 h-5 "
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
                   viewBox="0 0 22 21"
                 >
-                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
-                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+                  <path
+                    className="fill-sekunder"
+                    d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"
+                  />
+                  <path
+                    className="fill-sekunder"
+                    d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"
+                  />
                 </svg>
                 <span className="ms-3">Dashboard</span>
               </a>
