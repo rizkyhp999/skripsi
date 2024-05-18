69d637565d924a086fd2ef76067f2f46870fd3ef
rizkyhp999 <rizkyhutamaputra@gmail.com>
2024-05-19 02:57:12 +0700

    infogr

---
+++ src/components/atoms/gambar.tsx
@@ -0,0 +1,94 @@
+import Image from "next/image";
+import { useState } from "react";
+import Modal from "react-modal";
+
+interface InfografikProps {
+  gambar: string[];
+  altText: string;
+}
+
+const GambarInfografik: React.FC<InfografikProps> = ({ gambar, altText }) => {
+  const [gambarAktif, setGambarAktif] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleGambarClick = (index: number) => {
+    setGambarAktif(index);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  const handleSaveAllImages = () => {
+    gambar.forEach((namaFile) => {
+      const link = document.createElement("a");
+      link.href = `/infografik/${namaFile}`;
+      link.download = namaFile;
+      link.click();
+    });
+  };
+  const handleNextImage = () => {
+    setGambarAktif((gambarAktif + 1) % gambar.length);
+  };
+
+  const handlePrevImage = () => {
+    setGambarAktif((gambarAktif - 1 + gambar.length) % gambar.length);
+  };
+
+  return (
+    <div className="infografik-container  ">
+      <Image
+        src={`/infografik/${gambar[gambarAktif]}`}
+        alt={`${altText} - Gambar ${gambarAktif + 1}`}
+        width={250}
+        height={300}
+        onClick={() => handleGambarClick(gambarAktif)}
+        className=" shadow-xl border h-96" // Tambahkan kelas Tailwind h-96
+      />
+
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        contentLabel="Infografik Popup"
+        overlayClassName="fixed inset-0 bg-black/50"
+        className="bg-transparent outline-none border-none"
+      >
+        <button onClick={closeModal}>Tutup</button>
+        <button onClick={handleSaveAllImages}>Simpan Semua Gambar</button>
+        <div className="flex flex-row items-center justify-center relative ">
+          {gambarAktif > 0 && (
+            <Image
+              src={`/infografik/${gambar[gambarAktif - 1]}`}
+              alt="Preview gambar sebelumnya"
+              width={100}
+              height={150}
+              className="mr-4 opacity-50 hover:opacity-100 cursor-pointer"
+              onClick={handlePrevImage}
+            />
+          )}
+          <div className="">
+            <Image
+              key={gambarAktif}
+              src={`/infografik/${gambar[gambarAktif]}`}
+              alt={`${altText} - Gambar ${gambarAktif + 1}`}
+              width={500}
+              height={600}
+            />
+          </div>
+          {gambarAktif < gambar.length - 1 && (
+            <Image
+              src={`/infografik/${gambar[gambarAktif + 1]}`}
+              alt="Preview gambar selanjutnya"
+              width={100}
+              height={150}
+              className="ml-4 opacity-50 hover:opacity-100 cursor-pointer"
+              onClick={handleNextImage}
+            />
+          )}
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+export default GambarInfografik;
--- src/components/pages/infografik.tsx
+++ src/components/pages/infografik.tsx
@@ -1,9 +1,64 @@
+"use client";
+
 import React from "react";
 import Judul from "../atoms/text";
-export default function infografik() {
+import GambarInfografik from "../atoms/gambar";
+
+interface InfografikData {
+  gambar: string[];
+  altText: string;
+}
+
+export default function Infografik() {
+  const infografikData: InfografikData[] = [
+    {
+      gambar: [
+        "184_1645531041-1.png",
+        "184_1645531041-2.png",
+        "184_1645531041-3.png",
+        "184_1645531041-4.png",
+        "184_1645531041-5.png",
+        "184_1645531041-6.png",
+        "184_1645531041-7.png",
+      ],
+      altText: "Infografik pertama",
+    },
+    {
+      gambar: [
+        "783Leflet_Peta_Bahasa_2019-1.png",
+        "783Leflet_Peta_Bahasa_2019-2.png",
+        "783Leflet_Peta_Bahasa_2019-3.png",
+        "783Leflet_Peta_Bahasa_2019-4.png",
+      ], // Data infografik kedua
+      altText: "Infografik kedua",
+    },
+    {
+      gambar: [
+        "INFOGRAFIK_MB17-01-01.png",
+        "INFOGRAFIK_MB17-01-02.png",
+        "INFOGRAFIK_MB17-01-03.png",
+        "INFOGRAFIK_MB17-01-04.png",
+        "INFOGRAFIK_MB17-01-05.png",
+      ],
+      altText: "Infografik ketigas",
+    },
+    // Tambahkan data infografik lainnya sesuai kebutuhan
+  ];
+
   return (
     <div className="pt-10">
       <Judul classname="text-black">Infografik</Judul>
+      <div className="flex flex-wrap items-center justify-evenly mb-10 ">
+        {infografikData.map((data, index) => (
+          <div className="pt-10">
+            <GambarInfografik
+              key={index}
+              gambar={data.gambar}
+              altText={data.altText}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
