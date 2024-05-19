cf0a02a40f0530373638c4dc382bb5f0ce8a6387
rizkyhp999 <rizkyhutamaputra@gmail.com>
2024-05-19 15:30:53 +0700

    infografik

--- src/components/atoms/gambar.tsx
+++ src/components/atoms/gambar.tsx
@@ -33,11 +33,11 @@ const GambarInfografik: React.FC<InfografikProps> = ({
     });
   };
   const handleNextImage = () => {
-    setGambarAktif((gambarAktif + 1) % gambar.length);
+    setGambarAktif((gambarAktif + 1) % gambar.length); // Loop kembali ke awal jika mencapai akhir
   };
 
   const handlePrevImage = () => {
-    setGambarAktif((gambarAktif - 1 + gambar.length) % gambar.length);
+    setGambarAktif((gambarAktif - 1 + gambar.length) % gambar.length); // Loop kembali ke akhir jika mencapai awal
   };
 
   return (
@@ -55,59 +55,33 @@ const GambarInfografik: React.FC<InfografikProps> = ({
         isOpen={isModalOpen}
         onRequestClose={closeModal}
         contentLabel="Infografik Popup"
-        overlayClassName="flex justify-center items-center fixed inset-0 bg-black/50 z-50  "
+        overlayClassName="flex justify-center items-center fixed inset-0 bg-black/50 z-50   "
         className="bg-transparent outline-none border-none"
       >
         <div className="flex flex-row items-center justify-center relative ">
-          {gambarAktif > 0 && (
-            <div>
-              <Image
-                src={`/infografik/${gambar[gambarAktif - 1]}`}
-                alt="Preview gambar sebelumnya"
-                width={100}
-                height={150}
-                className="mr-4 opacity-50 hover:opacity-100 cursor-pointer hidden lg:block"
-                onClick={handlePrevImage}
-              />
-              <button
-                onClick={handlePrevImage}
-                className="mr-4 text-6xl lg:hidden text-white"
-              >
-                {"<"} {/* Tombol panah kiri */}
-              </button>
-            </div>
-          )}
+          <Image
+            src={`/infografik/${
+              gambar[(gambarAktif - 1 + gambar.length) % gambar.length]
+            }`}
+            alt="Preview gambar sebelumnya"
+            width={100}
+            height={150}
+            className="mr-10 opacity-50 hover:opacity-100 cursor-pointer hidden lg:block"
+            onClick={handlePrevImage}
+          />
+          <button
+            onClick={handlePrevImage}
+            className="mr-4 text-6xl lg:hidden text-white"
+          >
+            {"<"} {/* Tombol panah kiri */}
+          </button>
+
           <div className=" w-full max-w-md">
-            <div className="relative flex flex-row items-center justify-between mt-10">
-              <button
-                onClick={closeModal}
-                className="  flex flex-col text-xl text-white items-center justify-center"
-              >
-                <Image
-                  src={`/komponen/ikon/Tutup.svg`}
-                  alt="unduh"
-                  width={40}
-                  height={40}
-                ></Image>
-                Tutup
-              </button>
-              <button
-                onClick={handleSaveAllImages}
-                className=" flex flex-col text-xl text-white items-center justify-center "
-              >
-                <Image
-                  src={`/komponen/ikon/Unduh.svg`}
-                  alt="unduh"
-                  width={40}
-                  height={40}
-                ></Image>
-                Unduh
-              </button>
-            </div>
             <a
               href={`/infografik/${gambar[gambarAktif]}`}
               target="_blank"
               rel="noopener noreferrer"
+              className="flex items-center justify-center"
             >
               {" "}
               {/* Tambahkan tautan */}
@@ -116,34 +90,26 @@ const GambarInfografik: React.FC<InfografikProps> = ({
                 src={`/infografik/${gambar[gambarAktif]}`}
                 alt={`${altText} - Gambar ${gambarAktif + 1}`}
                 width={400}
-                height={600}
+                height={500}
                 priority={true}
-                style={{
-                  width: "auto",
-                  height: "auto",
-                }}
-                className="w-full h-auto"
               />
             </a>
           </div>
-          {gambarAktif < gambar.length - 1 && (
-            <div>
-              <Image
-                src={`/infografik/${gambar[gambarAktif + 1]}`}
-                alt="Preview gambar selanjutnya"
-                width={100}
-                height={150}
-                className="ml-4 opacity-50 hover:opacity-100 cursor-pointer hidden lg:block"
-                onClick={handleNextImage}
-              />
-              <button
-                onClick={handleNextImage}
-                className="ml-4 text-6xl lg:hidden text-white"
-              >
-                {">"} {/* Tombol panah kanan */}
-              </button>
-            </div>
-          )}
+
+          <Image
+            src={`/infografik/${gambar[(gambarAktif + 1) % gambar.length]}`}
+            alt="Preview gambar selanjutnya"
+            width={100}
+            height={150}
+            className="ml-10 opacity-50 hover:opacity-100 cursor-pointer hidden lg:block"
+            onClick={handleNextImage}
+          />
+          <button
+            onClick={handleNextImage}
+            className="ml-4 text-6xl lg:hidden text-white"
+          >
+            {">"} {/* Tombol panah kanan */}
+          </button>
         </div>
       </Modal>
     </div>
