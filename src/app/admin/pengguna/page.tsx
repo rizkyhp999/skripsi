b4827f8faf80b04bbfa52f4f2bb9650ce961a0c6
rizkyhp999 <rizkyhutamaputra@gmail.com>
2024-05-21 16:17:05 +0700

    login

--- src/app/admin/pengguna/page.tsx
+++ src/app/admin/pengguna/page.tsx
@@ -1,181 +1,149 @@
-import React from "react";
+"use client";
+import { useState, useEffect } from "react";
 import Admin from "@/components/templates/admin";
+import { ButtonBiru } from "@/components/molecules/button";
 
 export default function page() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch("/api/pengguna");
+        if (!res.ok) {
+          throw new Error("Gagal mengambil data pengguna");
+        }
+        const data = await res.json();
+        setUsers(data.data);
+      } catch (err) {
+        // setError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchData(); // Panggil fungsi fetchData sekali saat komponen dimuat
+  }, []); // Dependensi kosong [] memastikan useEffect hanya berjalan sekali
+
+  if (isLoading) return <p>Loading...</p>; // Tampilkan loading jika data belum siap
+  if (error) return <p>Error: {error}</p>; // Tampilkan pesan error jika ada
+  console.log(users);
   return (
     <>
       <Admin judul="Data Pengguna">
-        <div>
-          <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <div className="flex items-center justify-between flex-column flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4 bg-white">
-              <div>
-                <button
-                  id="dropdownActionButton"
-                  data-dropdown-toggle="dropdownAction"
-                  className="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5"
-                  type="button"
-                >
-                  <span className="sr-only">Action button</span>
-                  Action
-                  <svg
-                    className="w-2.5 h-2.5 ms-2.5"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 10 6"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="m1 1 4 4 4-4"
+        <div className="flex justify-end">
+          <ButtonBiru onClick={() => console.log("clicked")} classname="">
+            Tambah
+          </ButtonBiru>
+        </div>
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+              <tr>
+                <th scope="col" className="p-4">
+                  <div className="flex items-center">
+                    <input
+                      id="checkbox-all-search"
+                      type="checkbox"
+                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
                     />
-                  </svg>
-                </button>
-                <div
-                  id="dropdownAction"
-                  className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44"
-                >
-                  <ul
-                    className="py-1 text-sm text-gray-700"
-                    aria-labelledby="dropdownActionButton"
-                  >
-                    <li>
-                      <a href="#" className="block px-4 py-2 hover:bg-gray-100">
-                        Reward
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" className="block px-4 py-2 hover:bg-gray-100">
-                        Promote
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" className="block px-4 py-2 hover:bg-gray-100">
-                        Activate account
-                      </a>
-                    </li>
-                  </ul>
-                  <div className="py-1">
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                    >
-                      Delete User
-                    </a>
+                    <label htmlFor="checkbox-all-search" className="sr-only">
+                      checkbox
+                    </label>
                   </div>
-                </div>
-              </div>
-              <label htmlFor="table-search" className="sr-only">
-                Search
-              </label>
-              <div className="relative">
-                <div className="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
-                  <svg
-                    className="w-4 h-4 text-gray-500"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 20 20"
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Nama
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Position
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Status
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Terakhir Masuk
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Action
+                </th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {users.length > 0 &&
+                users.map((user: any) => (
+                  <tr
+                    className="bg-white border-b hover:bg-gray-50"
+                    key={user.id}
                   >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                    />
-                  </svg>
-                </div>
-                <input
-                  type="text"
-                  id="table-search-users"
-                  className="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
-                  placeholder="Search htmlFor users"
-                />
-              </div>
-            </div>
-            <table className="w-full text-sm text-left rtl:text-right text-gray-500">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50">
-                <tr>
-                  <th scope="col" className="p-4">
-                    <div className="flex items-center">
-                      <input
-                        id="checkbox-all-search"
-                        type="checkbox"
-                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
-                      />
-                      <label htmlFor="checkbox-all-search" className="sr-only">
-                        checkbox
-                      </label>
-                    </div>
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Nama
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Position
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Status
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Terakhir Masuk
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Action
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr className="bg-white border-b hover:bg-gray-50">
-                  <td className="w-4 p-4">
-                    <div className="flex items-center">
-                      <input
-                        id="checkbox-table-search-1"
-                        type="checkbox"
-                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
-                      />
-                      <label
-                        htmlFor="checkbox-table-search-1"
-                        className="sr-only"
-                      >
-                        checkbox
-                      </label>
-                    </div>
-                  </td>
-                  <th
-                    scope="row"
-                    className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap"
-                  >
-                    <div className="">
-                      <div className="text-base font-semibold">Rizky</div>
-                      <div className="font-normal text-gray-500">
-                        222011402@stis.ac.id
+                    <td className="w-4 p-4">
+                      <div className="flex items-center">
+                        <input
+                          id="checkbox-table-search-1"
+                          type="checkbox"
+                          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
+                        />
+                        <label
+                          htmlFor="checkbox-table-search-1"
+                          className="sr-only"
+                        >
+                          checkbox
+                        </label>
                       </div>
-                    </div>
-                  </th>
-                  <td className="px-6 py-4">Pengguna Admin</td>
-                  <td className="px-6 py-4">
-                    <div className="flex items-center">
-                      <div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div>
-                      Aktif
-                    </div>
-                  </td>
-                  <td className="px-6 py-4">17.04, 20 April 2024</td>
-                  <td className="px-6 py-4">
-                    <a
-                      href="#"
-                      className="font-medium text-blue-600 hover:underline"
+                    </td>
+                    <th
+                      scope="row"
+                      className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap"
                     >
-                      Edit Pengguna
-                    </a>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+                      <div className="">
+                        <div className="text-base font-semibold">
+                          {user.nama}
+                        </div>
+                        <div className="font-normal text-gray-500">
+                          {user.email}
+                        </div>
+                      </div>
+                    </th>
+                    <td className="px-6 py-4">{user.posisi}</td>
+                    <td className="px-6 py-4">
+                      <div className="flex items-center">
+                        <div
+                          className={`h-2.5 w-2.5 rounded-full me-2 ${
+                            user.status === "Aktif"
+                              ? "bg-green-500"
+                              : "bg-red-500"
+                          }`}
+                        ></div>
+                        {user.status}
+                      </div>
+                    </td>
+                    <td className="px-6 py-4">
+                      {new Date(
+                        user.terakhirmasuk.seconds * 1000
+                      ).toLocaleDateString("id-ID", {
+                        year: "numeric",
+                        month: "long",
+                        day: "numeric",
+                        hour: "numeric",
+                        minute: "numeric",
+                        second: "numeric",
+                      })}
+                    </td>
+                    <td className="px-6 py-4">
+                      <a
+                        href="#"
+                        className="font-medium text-blue-600 hover:underline"
+                      >
+                        Edit Pengguna
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
         </div>
       </Admin>
     </>
