c53d92a54dcb6c3ab383f5d5b7a3dd7ee6548e03
rizkyhp999 <rizkyhutamaputra@gmail.com>
2024-05-25 10:20:50 +0700

    subinde

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Carousel from "@/components/pages/carousel";
 import TextBeranda from "@/components/pages/textBeranda";
 import Peta from "@/components/pages/peta";
-import SubIndeks from "@/components/pages/subIndeks";
+import SubIndeks from "@/components/templates/subIndeks";
 import Infografik from "@/components/pages/infografik";
 export default function page() {
   return (
